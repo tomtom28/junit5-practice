ecd02ba87514feea6e645cf62348fa710b18b96e
Tommy Thompson <tomthompson828@gmail.com>
2020-03-08 12:40:50 -0400

    finished up all test cases

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -16,4 +16,16 @@ public class Calculator {
 		return a + b;
 	}
 
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+//	public int multiply(int a, int b) {
+//		return a * b;
+//	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
 }
--- src/test/java/com/example/project/CalculatorTests.java
+++ src/test/java/com/example/project/CalculatorTests.java
@@ -11,31 +11,114 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
+
+@DisplayName("Calculator")
 class CalculatorTests {
 
+	// Test 1 for Addition
 	@Test
 	@DisplayName("1 + 1 = 2")
-	void addsTwoNumbers() {
+	void addsOneAndTwo() {
 		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+		int expected = 2;
+		int actual = calculator.add(1, 1);
+		String messageIfFailure = "1 + 1 should equal 2";
+		assertEquals(expected, actual, messageIfFailure);
 	}
 
-	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@CsvSource({
-			"0,    1,   1",
-			"1,    2,   3",
-			"49,  51, 100",
-			"1,  100, 101"
-	})
-	void add(int first, int second, int expectedResult) {
+	// Test 2 for Addition
+	@Test
+	@DisplayName("-1 + 2 = 1")
+	void addsNegativeOneAndTwo() {
 		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
+		assertEquals(1, calculator.add(-1,2), "-1 + 2 should equal 1");
 	}
+
+
+	// -----------------------------------------------------------------------------------------------------------------
+
+
+	// Intentionally Skip an Unfinished Test Case for Multiply
+	@Test
+	@DisplayName("Skipped Test: 2 * 3 = 6")
+	@Ignore // <-- Just add the "Ignore" tag
+	void multiplyTwoAndThree() {
+		// TODO: Implement Test once multiply method is created
+	}
+
+
+	// -----------------------------------------------------------------------------------------------------------------
+
+
+	// Intentionally Fail an Unfinished Test Case for Multiply
+	@Test
+	@DisplayName("2 * 3 = 6")
+	void multiplyTwoAndThreeFailure() {
+		fail("Untested Case: 2 * 3 = 6");
+	}
+
+
+	// -----------------------------------------------------------------------------------------------------------------
+
+
+	// Two Combined Tests for Subtraction
+	@Nested
+	@DisplayName("Subtraction Operation")
+	class SubtractionTests {
+		@Test
+		@DisplayName("1 - 1 = 0")
+		void subtractOneAndOne() {
+			Calculator calculator = new Calculator();
+			assertEquals(0, calculator.subtract(1,1), "1 - 1 should equal 0");
+		}
+
+		@Test
+		@DisplayName("1 - (-2) = 3")
+		void subtractOneAndNegativeTwo() {
+			Calculator calculator = new Calculator();
+			assertEquals(3, calculator.subtract(1,-2), "1 - (-2) should equal 3");
+		}
+	}
+
+
+	// -----------------------------------------------------------------------------------------------------------------
+
+
+	// Three Combined Tests for Divide
+	@Nested
+	@DisplayName("Division Operation")
+	class DivisionTests {
+
+		@Test
+		@DisplayName("6 / 2 = 3")
+		void divideSixByTwo() {
+			Calculator calculator = new Calculator();
+			assertEquals(3, calculator.divide(6, 2), "6 / 2 should equal 3");
+		}
+
+		@Test
+		@DisplayName("1 / 2 = 0.5")
+		void divideOneByTwo() {
+			Calculator calculator = new Calculator();
+			// TODO: What do you think will happen?
+			assertEquals(0.5, calculator.divide(1, 2), "1 / 2 should equal 0.5");
+		}
+
+		@Test
+		@DisplayName("1 / 0 = ?")
+		@Ignore // <-- TODO: Implement test
+		void divideOneByZero() {
+			Calculator calculator = new Calculator();
+			// TODO: How do you think we can test this?
+		}
+
+	}
+
 }
